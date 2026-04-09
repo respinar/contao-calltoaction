a7c9f5e782285b8bcd604080fd85bad6d5fb786e
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-09 18:16:23 +0330

    Normalize code style and formatting

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license MIT
  */
 
-$GLOBALS["TL_DCA"]["tl_module"]["palettes"]["calltoaction"] = '
+$GLOBALS['TL_DCA']['tl_module']['palettes']['calltoaction'] = '
     {title_legend},name,type;
     {whatsapp_legend},ctaText,ctaIsVisible,ctaUrl,ctaTitle;
     {template_legend:hide},cssID,customTpl;
@@ -18,27 +18,27 @@ $GLOBALS["TL_DCA"]["tl_module"]["palettes"]["calltoaction"] = '
 ';
 
 // Define fields
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaIsVisible"] = [
-    "exclude" => true,
-    "inputType" => "checkbox",
-    "eval" => ["tl_class" => "w50 m12"],
-    "sql" => ['type' => 'boolean', 'default' => true],
+$GLOBALS['TL_DCA']['tl_module']['fields']['ctaIsVisible'] = [
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => ['tl_class' => 'w50 m12'],
+    'sql' => ['type' => 'boolean', 'default' => true],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaTitle"] = [
-    "inputType" => "text",
-    "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['ctaTitle'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaUrl"] = [
-    "inputType" => "text",
-    "eval" => ['mandatory'=>true, 'rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>2048, 'dcaPicker'=>true, 'tl_class'=>'w50 clr'],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['ctaUrl'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'rgxp' => 'url', 'decodeEntities' => true, 'maxlength' => 2048, 'dcaPicker' => true, 'tl_class' => 'w50 clr'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaText"] = [
-    "inputType" => "text",
-    "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['ctaText'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
