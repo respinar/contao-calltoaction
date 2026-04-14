f4b019a7ea3723bb2aa1e12b7e0e76378bb3dcf5
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-14 20:12:51 +0330

    Rename whatsapp legend to cta and extend URL length

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['calltoaction'] = '
     {title_legend},name,type;
-    {whatsapp_legend},ctaText,ctaIsVisible,ctaUrl,ctaTitle;
+    {cta_legend},ctaText,ctaIsVisible,ctaUrl,ctaTitle;
     {template_legend:hide},cssID,customTpl;
     {protected_legend:hide},protected;
 ';
@@ -34,7 +34,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['ctaTitle'] = [
 $GLOBALS['TL_DCA']['tl_module']['fields']['ctaUrl'] = [
     'inputType' => 'text',
     'eval' => ['mandatory' => true, 'rgxp' => 'url', 'decodeEntities' => true, 'maxlength' => 2048, 'dcaPicker' => true, 'tl_class' => 'w50 clr'],
-    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
+    'sql' => ['type' => 'string', 'length' => 2048, 'default' => ''],
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['ctaText'] = [
