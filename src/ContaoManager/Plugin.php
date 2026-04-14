75160b7f236766713ca135c42e6b4004c8d000fa
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-14 20:36:03 +0330

    Rename bundle namespace to CallToAction

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,20 +10,20 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\CalltoactionBundle\ContaoManager;
+namespace Respinar\CallToActionBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Respinar\CalltoactionBundle\RespinarCalltoactionBundle;
+use Respinar\CallToActionBundle\RespinarCallToActionBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(RespinarCalltoactionBundle::class)
+            BundleConfig::create(RespinarCallToActionBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
